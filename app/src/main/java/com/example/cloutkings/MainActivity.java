1479b39217d858902e17ff54903b8a023d7b4b5c
jlf4509 <jlf4509@rit.edu>
2019-12-20 16:07:34 -0500

    made a lot of progress to the profiles, having issues with the image not showing but right now I'm changing the layout a bit

--- app/src/main/java/com/example/cloutkings/MainActivity.java
+++ app/src/main/java/com/example/cloutkings/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cloutkings;
 
 import android.os.Bundle;
 
+import com.example.cloutkings.ui.ProfileAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +10,21 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Recycler - created in xml
+    private RecyclerView mRecyclerView;
+    // bridge between our data and our recycler ^^
+    private RecyclerView.Adapter mAdapter;
+    // aligns our profiles in the list
+    private RecyclerView.LayoutManager mLayoutManager;
+    // Person Name
+    private ArrayList<Profile> listOfProfiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,21 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        // creating the hasmap of profiles for fast access
+        this.listOfProfiles = new ArrayList<>();
+        Person current = new Person("Lionel Messi", "https://www.instagram.com/leomessi/?hl=en");
+        this.listOfProfiles.add(new Profile(R.drawable.messi_profile_pic_background, "Messi", "Professional Soccer Player", current));
+        this.mRecyclerView = findViewById(R.id.recyclerView);
+        this.mRecyclerView.setHasFixedSize(true);
+        this.mLayoutManager = new LinearLayoutManager(this);
+        this.mAdapter = new ProfileAdapter(this.listOfProfiles);
+        this.mRecyclerView.setLayoutManager(this.mLayoutManager);
+        this.mRecyclerView.setAdapter(this.mAdapter);
     }
+
+//    public void addProfile() {
+//        Person current = new Person("Lionel Messi", "https://www.instagram.com/leomessi/?hl=en");
+//        this.listOfProfiles.add(new Profile(R.drawable.messi_profile_pic_background, "Messi", "Professional Soccer Player", current));
+//    }
 
 }
